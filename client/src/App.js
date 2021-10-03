8186e173aad28ae100040884bdf8d37943ee4920
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-10-04 00:19:24 +0200

    28.4

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,85 @@
 import React from 'react';
- 
+import io from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
+
 class App extends React.Component {
- 
+
+  state = {
+    tasks: [],
+    taskName: '',
+  }
+
+  componentDidMount() {
+    this.socket = io.connect('http://localhost:8000', {
+      transports: ['websocket'],
+    });
+    this.socket.on('addTask', task => this.addTask(task));
+    this.socket.on('removeTask', id => this.removeTask(id));
+    this.socket.on('updateData', tasks => this.updateTasks(tasks));
+  }
+
+  updateTasks = newTasks => {
+    this.setState({ tasks: newTasks });
+  }
+
+  addTask = task => {
+    //this.setState({ tasks: this.state.tasks.concat(task) });
+    this.setState({ tasks: [...this.state.tasks, task] });
+  }
+
+  submitForm = e => {
+    e.preventDefault();
+    const newTask = { id: uuidv4(), name: this.state.taskName };
+    this.addTask(newTask);
+    this.socket.emit('addTask', newTask);
+    this.setState({ taskName: '' });
+  }
+
+  removeTask = (id, local) => {
+    this.setState({ tasks: this.state.tasks.filter((item => item.id !== id)) });
+    if(local) {this.socket.emit('removeTask', id)};
+  }
+
   render() {
+    const { tasks, taskName } = this.state;
+
     return (
       <div className="App">
-     
         <header>
           <h1>ToDoList.app</h1>
         </header>
-     
+
         <section className="tasks-section" id="tasks-section">
           <h2>Tasks</h2>
-     
+
           <ul className="tasks-section__list" id="tasks-list">
-            <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-            <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+            {tasks.map(item => (
+              <li key={item.id} className="task">{item.name}
+                <button onClick={() => this.removeTask(item.id, true)} className="btn btn--red">Remove</button>
+              </li>
+            ))}
           </ul>
-     
-          <form id="add-task-form">
-            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
+
+          <form
+            id="add-task-form"
+            onSubmit={e => this.submitForm(e)}
+          >
+            <input
+              className="text-input"
+              autoComplete="off"
+              type="text"
+              placeholder="Type your description"
+              id="task-name"
+              value={taskName}
+              onChange={e => this.setState({ taskName: e.target.value })}
+            />
             <button className="btn" type="submit">Add</button>
           </form>
-     
+
         </section>
       </div>
     );
-    
   };
- 
 };
- 
+
 export default App;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const socket = require('socket.io');
 
- 
 const app = express();
 
 const tasks = [];
@@ -9,7 +8,7 @@ const tasks = [];
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
- 
+
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
 });
@@ -17,35 +16,17 @@ app.use((req, res) => {
 const io = socket(server);
 
 io.on('connection', (socket) => {
+  console.log('New client! Its id: ' + socket.id);
+  socket.emit('updateData', tasks);
 
-    socket.emit('updateData', tasks);
+  socket.on('addTask', taskName => {
+    tasks.push(taskName);
+    socket.broadcast.emit('addTask', taskName);
+  });
 
-    socket.on('addTask', (taskName) => {
-        tasks.push(taskName);
-        socket.broadcast.emit('addTask', taskName);
-    });
-    socket.on('removeTask', (id) => {
-        const indexToRemove = tasks.indexOf(tasks.find(task => task.id === id ));
-        tasks.splice(indexToRemove, 1);
-        socket.broadcast.emit('removeTask', id);
-    });
-
-    
-    
-    socket.on('message', (message) => {
-        console.log('Oh, I\'ve got something from ' + socket.id);
-        messages.push(message);
-        socket.broadcast.emit('message', message);
-      });
-      
-    socket.on('disconnect', () => { 
-        console.log('Oh, socket ' + socket.id + ' has left');
-        const user = users.find(object => object.id === socket.id)
-        const index = users.indexOf(user);
-        users.splice(index, 1);
-        socket.broadcast.emit('message', {author: 'Chat Bot', content: `${user.name} has left the conversation` });
-
-    });
-    console.log('I\'ve added a listener on message event \n');
+  socket.on('removeTask', id => {
+    const indexToRemove = tasks.indexOf(tasks.find(task =>task.id === id ));
+    tasks.splice(indexToRemove, 1);
+    socket.broadcast.emit('removeTask', id);
+  });
 });
-  
